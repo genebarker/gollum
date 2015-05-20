cf4d0f8a8781f40c99c3a9f3986c3843196709f6
Eugene F. Barker <genebarker@gmail.com>
2015-05-21 11:42:38 +1300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+#----------------------------------------------------------------------
+# deb8gollum - Gollum wiki on Debian Jessie
+#              (includes option to run as a rack application)
+#
+# Version 0.4
+#
+# For usage info, just run the image without a command:
+#   $ docker run --rm genebarker/deb8gollum
+#
+# For a rack app, see the example (gollumrack.rb), and be sure to
+# append its required packages and gems to the respective RUN commands
+# below.
+#----------------------------------------------------------------------
+
+FROM debian:8.0
+
+MAINTAINER Eugene F. Barker <genebarker@gmail.com>
+
+# install dependencies
+RUN apt-get update && apt-get install -y \
+  ruby \
+  ruby-dev \
+  libz-dev \
+  libicu-dev \
+  build-essential \
+  git
+
+# install gollum
+RUN gem install gollum
+
+# expose default port
+EXPOSE 4567
+
+# initialize wiki content
+RUN mkdir /root/wiki
+RUN git init /root/wiki
+
+# copy in smart entrypoint script
+COPY ./entrypoint.sh /
+# set entry point
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+echo "initialize deb8gollum container"
+echo "set working directory to: /root/wiki"
+cd /root/wiki
+if [ "$#" != "0" ]; then
+    CMD="$1"
+    if [ "$CMD" == 'gollum' ]; then
+        echo "perform standard gollum start using following command:"
+        echo "   $ $@"
+        exec "$@"
+        exit
+    elif [ "$CMD" == 'ruby' ]; then
+        echo "perform a rack gollum start using following command"
+        echo "   $ $@"
+        if [ "$#" == "1" ]; then
+            echo "error: no application file provided."
+            exit 1
+        fi
+        if [ ! -f "$2" ]; then
+            echo "error: application file not found."
+            exit 1
+        fi
+        exec "$@"
+        exit
+    elif [ $CMD == 'help' ]; then
+        echo "show help"
+    else
+        exec "$@"
+        exit
+    fi
+fi
+
+echo
+echo "usage: genebarker/deb8gollum <command> [<args>]"
+echo
+echo "The available commands are:"
+echo "   gollum [<args>]  Run gollum using given arguments"
+echo "   ruby FILE        Run gollum FILE as a rack application"
+echo "                    (file must be located in /root/wiki)"
+echo "   help             Display this usage guide"
+echo
+echo "The wiki content is a git repository located at:"
+echo "   /root/wiki"
+echo
+echo "To use a wiki repository on the host, mount it, i.e:"
+echo "   $ docker run -d -p 4567:4567 \\"
+echo "       -v /home/elvis/mywiki:/root/wiki \\"
+echo "       genebarker/deb8gollum gollum"
+echo
+echo "To run gollum as a rack application, place the app's files in"
+echo "the wiki repository, then run it, i.e.:"
+echo "   $ docker run -d -p 4567:4567 \\"
+echo "       -v /home/elvis/mywiki:/root/wiki \\"
+echo "       genebarker/deb8gollum ruby myapp.rb"
+echo
+echo "To bypass script, just enter desired command, i.e.:"
+echo "   $ docker run -i -t genebarker/deb8gollum /bin/bash"
+echo
+exit 0
